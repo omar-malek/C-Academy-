86a29536a6bef2d7ba1b384e9fb51b1f005cb26f
omar.abdelmalek <omar.abdelmalek@>
2020-12-01 00:08:11 +0100

    add

--- LAB06-- command line arguments/main.c
+++ LAB06-- command line arguments/main.c
@@ -8,8 +8,8 @@ int main (int argc , char *argv[])
     char *argument1 =argv[0];
     char *argument2 =argv[1];
 
-    printf("Number of arguments  %d",numberOfArguments );
-    printf("Argument 1 is the program name : %s",argument1);
-    printf("Argument 2 is the program name : %s",argument2);
+    printf("Number of arguments  %d \n",numberOfArguments );
+    printf("Argument 1 is the program name : %s\n",argument1);
+    printf("Argument 2 is the program name : %s\n",argument2);
     return 0;
 }
