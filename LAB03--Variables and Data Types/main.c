336634c02e696436b6c22a06d442da1bbe530f2f
omar.abdelmalek <omar.abdelmalek@>
2020-11-30 23:20:56 +0100

    add programe

--- LAB03--Variables and Data Types/main.c
+++ LAB03--Variables and Data Types/main.c
@@ -9,12 +9,13 @@
 - initializing variable   ==> int x =21
 - variable type :
         int -------> 1 bytes  ---> signed
-        float -------> 4 bytes  ---->
-        double -------> bytes ---->  used when float not sufficient
-        char -------> 2 bytes
+        char -------> 1 bytes
         bool -------> 1 bytes
+        float -------> 4 bytes  ---->
+        double -------> 8 bytes ---->  used when float not sufficient
 
 
+        3 adjective keyword for int  short , long, unsigned
 */
 
 
@@ -29,8 +30,11 @@ int main ()
     float  J = 23.333;
     double  d= 55.5555555555;
     _Bool  mybool =1;//in C89
-     bool  boolVariable = true;  // in C99
+    bool  boolVariable = true;  // in C99
+    short int h;
+    long double ld;
 
+    printf(" the size = %d \n\n", sizeof(int));
 
 
     return 0;
---
+++ LAB04-- Enums/main.c
@@ -0,0 +1,44 @@
+
+#include <stdio.h>
+#include <stdint.h>
+#include <stdbool.h>
+
+/* Notes
+
+Escape characters
+
+\a	07	Alert (Beep, Bell) (added in C89)[1]
+\b	08	Backspace
+\enote 1	1B	Escape character
+\f	0C	Formfeed Page Break
+\n	0A	Newline (Line Feed); see notes below
+\r	0D	Carriage Return
+\t	09	Horizontal Tab
+\v	0B	Vertical Tab
+\\	5C	Backslash
+\'	27	Apostrophe or single quotation mark
+\"	22	Double quotation mark
+\?	3F	Question mark (used to avoid trigraphs)
+*/
+
+
+
+enum color { red, yellow , blue , black , green };
+char  broild;
+
+
+int main(void)
+{
+    printf("#############################################\n");
+    printf("\n##########  Variable and Data Types  ########\n");
+    printf("\n#############################################\n\n\n");
+
+enum color  my_color = black;
+
+    broild ='T';
+    printf(" value = %d\n", my_color);
+
+    printf(" text= \"%s \" \a \n", &broild);
+
+return 0;
+}
