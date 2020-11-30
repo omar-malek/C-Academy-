036b78d4c669ff4749aba500ef0746f42844331f
omar.abdelmalek <omar.abdelmalek@>
2020-12-01 00:50:55 +0100

    add

---
+++ LAB07-- Operators/main.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "stdbool.h"
+
+/**
+Arithmetic operator  + - * / % ++ --
+Logical operator  &&   ||  !
+Assignment operators  = += -= *=  /=  %/  <<=  >>=  &=  ^=  |=
+Relational operator == != > < >=  <=
+
+*/
+
+
+
+
+int main()
+{
+    /*Arithmetic operator*/
+    /*
+    int a= 33;
+    int b = 15;
+    int  result =0;
+    result=a/b; */
+
+
+    /*Logical  operator*/
+    /*
+    int a= true;
+    int b = false;
+    int  result = a||b;
+    printf("Result  is %d\n",!a);
+    */
+
+    /*Assignment operators*/
+    int a= true;
+    int b = false;
+    int  result = a||b;
+
+    return 0;
+}
+
