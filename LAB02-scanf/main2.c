dae4cecabb6094bf738f23264f407ce1c93fedc9
omar.abdelmalek <omar.abdelmalek@>
2020-11-30 19:40:11 +0100

    Variables and Data Types

---
+++ LAB02-scanf/main2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+
+int main()
+
+{
+        double x;
+        scanf("%lf",&x);
+    return 0;
+}
---
+++ LAB03--Variables and Data Types/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdint.h>
+
+
+
+int main ()
+{
+    printf("\n##########  Variable and Data Types  ########\n");
+
+    return 0;
+}
