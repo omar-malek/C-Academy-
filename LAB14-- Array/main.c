58cc9074199875002ed0b777e3cba9dfaec70068
omar.abdelmalek <omar.abdelmalek@>
2020-12-03 00:22:56 +0100

    add files

---
+++ LAB14-- Array/main.c
@@ -0,0 +1,16 @@
+#include  <stdio.h>
+#include <stdint.h>
+
+
+
+
+int main(){
+
+int number[3][4]={
+					{10,20,30,40}, //Value for the first row
+					{10,20,30,40}, //Value for the second row
+					{45,48,60,50}  //Value for the third row
+				};
+
+return 0;
+}
