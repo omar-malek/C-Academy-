8a6021988df873223b660e968cf17adfb046577d
omar.abdelmalek <omar.abdelmalek@>
2020-12-03 09:56:48 +0100

    add files

--- Challenge --Generat e Prime numbers/main.c
+++ Challenge --Generat e Prime numbers/main.c
@@ -1,39 +1,32 @@
+
 #include <stdio.h>
-#include <stdlib.h>
+#include  <stdint.h>
 #include <stdbool.h>
 
-int main()
+
+
+
+int mai()
+
 {
     int p;
     int i;
-
-    int primes[50] = {0};
-    int primeIndex = 2;
-
     bool isPrime;
 
-    // hardcode prime numbers
-    primes[0] = 2;
-    primes[1] = 3;
+    int primes [50]= {0};
+    int primeIndex =2;
 
-    for(p = 5; p <= 100; p = p + 2)
+    //hardcode prime number
+    primes[0]=2;
+    primes[1]=3;
+
+    for ( p=5; p<100; p=p+2)
     {
-        isPrime = true;
 
-        for (i = 1; isPrime && p / primes[i] >= primes[i]; ++i)
-            if (p % primes[i] == 0)
-                isPrime = false;
-
-        if (isPrime == true)
-        {
-            primes[primeIndex] = p;
-            ++primeIndex;
-        }
+        isPrime =true;
+        for(i=0; isPrime && p / primes[i]>= primes)
     }
 
-    for ( i = 0;  i < primeIndex;  ++i )
-         printf ("%i  ", primes[i]);
 
-    printf("\n");
     return 0;
 }
---
+++ LAB15--String/main.c
@@ -0,0 +1,23 @@
+#include <stdint.h>
+#include <stdio.h>
+#include <string.h>
+
+
+
+
+int main(){
+  char str[80]="Hello how are you-my name is-omar";
+  const char s[2]="-";
+  char* token;
+  /*get the first token*/
+  token= strtok(str,s);
+
+  /*walk through other tokens*/
+  while(token!=NULL){
+    printf("%s\n", token);
+    token =strtok(NULL,s);
+  }
+
+return 0;
+
+}
