e73a51ee2f59fb31d8eee5a9d84b5739d60ecb08
omar.abdelmalek <omar.abdelmalek@>
2020-12-03 12:07:00 +0100

    add file structure

---
+++ LAB16-- structure/main.c
@@ -0,0 +1,43 @@
+
+
+#include <stdio.h>
+#include <stdint.h>
+
+
+
+
+
+
+int main()
+{
+
+ return 0;
+}
+
+
+
+
+
+///*declaration  of structure */
+//struct date{
+//int month;
+//int day;
+//int year;
+//};
+//int main(){
+//
+///*today is variable of type date*/
+////struct date today={7,2,2015};
+//
+//struct date today=
+//{
+// .month =8,
+// .day=13,
+// .year=2020
+//};
+//printf("%d", today.month);
+//
+//
+//
+//return 0;
+//}
