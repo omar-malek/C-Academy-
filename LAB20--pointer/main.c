b0e786eb39a2aa48c3ccbcce5fc63d594dfb89c3
omar.abdelmalek <omar.abdelmalek@>
2020-12-04 01:58:24 +0100

    upload notes

--- LAB20--pointer/main.c
+++ LAB20--pointer/main.c
@@ -1,12 +1,17 @@
-
 #include <stdio.h>
-#include <stdint.h>
+#include <stdlib.h>
 
+int main()
+{
+    int num = 150;
+    int *pNum = NULL;
 
+    pNum = &num;
 
+    printf("num address is:  %p\n", &num);
+    printf("Address of pNum: %p\n", &pNum);
+    printf("value of the pNum: %p\n", pNum);
+    printf("value of what pNum is pointing to: %d\n", *pNum);
 
-int main(){
-
-
-return 0;
+    return 0;
 }
