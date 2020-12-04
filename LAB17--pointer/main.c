839ca32c651e854915b8a2ed77877462f70256a7
omar.abdelmalek <omar.abdelmalek@>
2020-12-04 17:37:20 +0100

    update files

---
+++ LAB17--pointer/main.c
@@ -0,0 +1,22 @@
+#include "stdint.h"
+#include "stdio.h"
+
+
+
+
+int main(){
+
+
+
+printf("hello world\n");
+printf("hello world\n");
+printf("hello world\n");
+printf("hello world\n");
+
+printf("hello world\n");
+
+
+
+return 0;
+
+}
